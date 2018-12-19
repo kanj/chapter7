13c2527e53182a976d14e4b9a6facda99c5567d0
kanj <knjterrell@mybluelight.com>
2018-12-19 15:07:17 -0800

    Adding new modules

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -1,10 +1,5 @@
 import os
 def run(**args):
-print"[*} in dirlister module"
-files=os.listdir("."
-EOF	
-cat <<EOF > trojan/modules/environment.py 
-import os
-def run(**args)
-print "[*] in environment mode"
-return str(os.environ)
+	print"[*} in dirlister module"
+	files=os.listdir(".")
+
